6a60612de9c57d6bc7b3a81c523a79cab6cee6c8
Hubert Suprunowicz <hubertsuprunowicz@gmail.com>
2024-03-17 12:24:15 +0100

    Add basic funcionality and assets

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
+// import * as PIXI from "./pixi";
+
+const LEFT = -1;
+const RIGHT = 1;
+
 (async () => {
   const app = new PIXI.Application();
   await app.init();
   document.body.appendChild(app.canvas);
 
-  const texture = await PIXI.Assets.load("./assets/bunny.png");
-  const bunny = new PIXI.Sprite(texture);
+  const playerTexture = await PIXI.Assets.load("./assets/player.png");
+  const player = new PIXI.Sprite(playerTexture);
+  player.x = app.renderer.width / 2;
+  player.y = app.renderer.height - player.height;
+  app.stage.addChild(player);
 
-  bunny.x = app.renderer.width / 2;
-  bunny.y = app.renderer.height / 2;
+  const enemyTexture = await PIXI.Assets.load("./assets/enemy.png");
+  const enemy = new PIXI.Sprite(enemyTexture);
+  enemy.x = app.renderer.width / 2;
+  enemy.y = app.renderer.height / 2;
+  app.stage.addChild(enemy);
 
-  bunny.anchor.x = 0.5;
-  bunny.anchor.y = 0.5;
-
-  app.stage.addChild(bunny);
+  let direction = RIGHT;
+  let enemySpeed = 2;
 
   app.ticker.add(() => {
-    bunny.rotation += 0.01;
+    if (enemy.x + enemy.width > app.renderer.width) {
+      direction = LEFT;
+    }
+
+    if (enemy.x < 0) {
+      direction = RIGHT;
+    }
+
+    enemy.position.x += enemySpeed * direction;
   });
 })();
